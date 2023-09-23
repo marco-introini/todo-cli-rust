2babbde2cbf5daef890caccecf66fdec3904dfc2
Marco "Mint" Introini <mint.dev@pm.me>
2023-09-23 20:12:06 +0200

    add commands

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,19 @@
+use std::{env, process};
+
 fn main() {
-    println!("Hello, world!");
+    let parametri: Vec<String> = env::args().collect();
+
+    let comando = parametri.get(1).unwrap_or_else(|| {
+        help_message();
+        process::exit(0);
+    });
+    dbg!(comando);
+
+}
+
+
+fn help_message() -> () {
+    println!("Uso: <comando> <opzione>");
+    println!();
+    println!("Comando: add <stringa> o list");
 }
